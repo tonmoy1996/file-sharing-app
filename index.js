3e6c43a8f390db50157500b2b074d046cd677d5e
tonmoy1996 <saha.tonmoy1996@gmail.com>
2022-03-26 01:50:44 +0600

    add slash

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ require("./config/db")();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.get("/",(req,res)=>{
+    res.send("working");
+})
 
 //register Routes
 
